b3127be4430dd9372493a6f883a2de2df96ac333
Noelle Hemphill <noelle.hemphill@gmail.com>
2023-06-01 14:28:21 -0600

    Add new condition

--- plants.rb
+++ plants.rb
@@ -1,8 +1,10 @@
-gets_sunshine = true
-ounces_of_water = 10
+gets_sunshine = false
+ounces_of_water = 9
 
 if gets_sunshine == true && ounces_of_water >= 10
     puts "thrive"
+elsif gets_sunshine == true || ounces_of_water >= 10
+    puts "Half dead...?"
 else
     puts "die"
 end
